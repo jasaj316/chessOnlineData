dcfefcef6651d08abac8c5468741452a44d219f0
Justin Smith <jasaj316@gmail.com>
2026-05-06 19:43:24 -0400

    added set, which made things stay as they are

--- functions/sessionData.js
+++ functions/sessionData.js
@@ -171,8 +171,8 @@ let data = {
   }
 }
 exports.handler = async event => {
-  const { wPlayerName, wp1, wp2, wp3, wp4, wp5, wp6, wp7, wp8, wr1, wr2, wn1, wn2, wb1, wb2, wq, wk, bPlayerName, bp1, bp2, bp3, bp4, bp5, bp6, bp7, bp8, br1, br2, bn1, bn2, bb1, bb2, bq, bk, mobile } = event.queryStringParameters;
-  if (mobile) {
+  const { wPlayerName, wp1, wp2, wp3, wp4, wp5, wp6, wp7, wp8, wr1, wr2, wn1, wn2, wb1, wb2, wq, wk, bPlayerName, bp1, bp2, bp3, bp4, bp5, bp6, bp7, bp8, br1, br2, bn1, bn2, bb1, bb2, bq, bk, set } = event.queryStringParameters;
+  if (set) {
   data = {
     White: {
       PlayerName: `${wPlayerName}`,
