e7850eeed8deeeb1cea4dc94834bc84d9aa74acd
Justin Smith <jasaj316@gmail.com>
2026-05-06 19:41:57 -0400

    Update sessionData.js

--- functions/sessionData.js
+++ functions/sessionData.js
@@ -171,7 +171,8 @@ let data = {
   }
 }
 exports.handler = async event => {
-  const { wPlayerName, wp1, wp2, wp3, wp4, wp5, wp6, wp7, wp8, wr1, wr2, wn1, wn2, wb1, wb2, wq, wk, bPlayerName, bp1, bp2, bp3, bp4, bp5, bp6, bp7, bp8, br1, br2, bn1, bn2, bb1, bb2, bq, bk } = event.queryStringParameters;
+  const { wPlayerName, wp1, wp2, wp3, wp4, wp5, wp6, wp7, wp8, wr1, wr2, wn1, wn2, wb1, wb2, wq, wk, bPlayerName, bp1, bp2, bp3, bp4, bp5, bp6, bp7, bp8, br1, br2, bn1, bn2, bb1, bb2, bq, bk, mobile } = event.queryStringParameters;
+  if (mobile) {
   data = {
     White: {
       PlayerName: `${wPlayerName}`,
@@ -344,6 +345,7 @@ exports.handler = async event => {
       ]
     }
   };
+  }
   const json = JSON.stringify(data);
   return {
     statusCode: 200,
