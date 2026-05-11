cbfe97ca1c719c44b76319e7e3efac853df30e82
Jsmith221 <jsmth216@gmail.com>
2026-05-11 17:38:10 -0400

    set a hidden position when objects load

--- main.js
+++ main.js
@@ -20,9 +20,87 @@ document.body.appendChild(renderer.domElement);
 
 //create and add objects
 //#region
+//object variables
 let glb, wp1Glb, wp2Glb, wp3Glb, wp4Glb, wp5Glb, wp6Glb, wp7Glb, wp8Glb, wr1Glb, wr2Glb, wn1Glb, wn2Glb, wb1Glb, wb2Glb, wqGlb, wkGlb, bp1Glb, bp2Glb, bp3Glb, bp4Glb, bp5Glb, bp6Glb, bp7Glb, bp8Glb, br1Glb, br2Glb, bn1Glb, bn2Glb, bb1Glb, bb2Glb, bqGlb, bkGlb;
 const loader = new GLTFLoader();
-//white pawns
+
+//set positions of everything using localData
+function setAllPos(hide) {
+  //white
+  if (hide) {
+    wp1Glb.position.set(-99999, 99999, 99999);
+    wp2Glb.position.set(-99999, 99999, 99999);
+    wp3Glb.position.set(-99999, 99999, 99999);
+    wp4Glb.position.set(-99999, 99999, 99999);
+    wp5Glb.position.set(-99999, 99999, 99999);
+    wp6Glb.position.set(-99999, 99999, 99999);
+    wp7Glb.position.set(-99999, 99999, 99999);
+    wp8Glb.position.set(-99999, 99999, 99999);
+    wr1Glb.position.set(-99999, 99999, 99999);
+    wr2Glb.position.set(-99999, 99999, 99999);
+    wn1Glb.position.set(-99999, 99999, 99999);
+    wn2Glb.position.set(-99999, 99999, 99999);
+    wb1Glb.position.set(-99999, 99999, 99999);
+    wb2Glb.position.set(-99999, 99999, 99999);
+    wqGlb.position.set(-99999, 99999, 99999);
+    wkGlb.position.set(-99999, 99999, 99999);
+    //black
+    bp1Glb.position.set(-99999, 99999, 99999);
+    bp2Glb.position.set(-99999, 99999, 99999);
+    bp3Glb.position.set(-99999, 99999, 99999);
+    bp4Glb.position.set(-99999, 99999, 99999);
+    bp5Glb.position.set(-99999, 99999, 99999);
+    bp6Glb.position.set(-99999, 99999, 99999);
+    bp7Glb.position.set(-99999, 99999, 99999);
+    bp8Glb.position.set(-99999, 99999, 99999);
+    br1Glb.position.set(-99999, 99999, 99999);
+    br2Glb.position.set(-99999, 99999, 99999);
+    bn1Glb.position.set(-99999, 99999, 99999);
+    bn2Glb.position.set(-99999, 99999, 99999);
+    bb1Glb.position.set(-99999, 99999, 99999);
+    bb2Glb.position.set(-99999, 99999, 99999);
+    bqGlb.position.set(-99999, 99999, 99999);
+    bkGlb.position.set(-99999, 99999, 99999);
+  }
+  else {
+    wp1Glb.position.set(-70, 0, 50);
+    wp2Glb.position.set(-50, 0, 50);
+    wp3Glb.position.set(-30, 0, 50);
+    wp4Glb.position.set(-10, 0, 50);
+    wp5Glb.position.set(10, 0, 50);
+    wp6Glb.position.set(30, 0, 50);
+    wp7Glb.position.set(50, 0, 50);
+    wp8Glb.position.set(70, 0, 50);
+    wr1Glb.position.set(-70, 0, 70);
+    wr2Glb.position.set(70, 0, 70);
+    wn1Glb.position.set(-50, 0, 70);
+    wn2Glb.position.set(50, 0, 70);
+    wb1Glb.position.set(-30, 0, 70);
+    wb2Glb.position.set(30, 0, 70);
+    wqGlb.position.set(10, 0, 70);
+    wkGlb.position.set(-10, 0, 70);
+    //black
+    bp1Glb.position.set(-70, 0, -50);
+    bp2Glb.position.set(-50, 0, -50);
+    bp3Glb.position.set(-30, 0, -50);
+    bp4Glb.position.set(-10, 0, -50);
+    bp5Glb.position.set(10, 0, -50);
+    bp6Glb.position.set(30, 0, -50);
+    bp7Glb.position.set(50, 0, -50);
+    bp8Glb.position.set(70, 0, -50);
+    br1Glb.position.set(-70, 0, -70);
+    br2Glb.position.set(70, 0, -70);
+    bn1Glb.position.set(-50, 0, -70);
+    bn2Glb.position.set(50, 0, -70);
+    bb1Glb.position.set(-30, 0, -70);
+    bb2Glb.position.set(30, 0, -70);
+    bqGlb.position.set(10, 0, -70);
+    bkGlb.position.set(-10, 0, -70);
+  }
+}
+
+//loads all pieces into the scene
+//white
 glb = await loader.loadAsync('public/pawn.glb');
 scene.add(wp1Glb = glb.scene);
 glb = await loader.loadAsync('public/pawn.glb');
@@ -39,7 +117,23 @@ glb = await loader.loadAsync('public/pawn.glb');
 scene.add(wp7Glb = glb.scene);
 glb = await loader.loadAsync('public/pawn.glb');
 scene.add(wp8Glb = glb.scene);
-//black pawns
+glb = await loader.loadAsync('public/rook.glb');
+scene.add(wr1Glb = glb.scene);
+glb = await loader.loadAsync('public/rook.glb');
+scene.add(wr2Glb = glb.scene);
+glb = await loader.loadAsync('public/knight.glb');
+scene.add(wn1Glb = glb.scene);
+glb = await loader.loadAsync('public/knight.glb');
+scene.add(wn2Glb = glb.scene);
+glb = await loader.loadAsync('public/bishop.glb');
+scene.add(wb1Glb = glb.scene);
+glb = await loader.loadAsync('public/bishop.glb');
+scene.add(wb2Glb = glb.scene);
+glb = await loader.loadAsync('public/queen.glb');
+scene.add(wqGlb = glb.scene);
+glb = await loader.loadAsync('public/king.glb');
+scene.add(wkGlb = glb.scene);
+//black
 glb = await loader.loadAsync('public/pawn.glb');
 scene.add(bp1Glb = glb.scene);
 glb = await loader.loadAsync('public/pawn.glb');
@@ -56,31 +150,23 @@ glb = await loader.loadAsync('public/pawn.glb');
 scene.add(bp7Glb = glb.scene);
 glb = await loader.loadAsync('public/pawn.glb');
 scene.add(bp8Glb = glb.scene);
-
-//#endregion
-
-//set positions of everything
-function setAllPos() {
-  //white pawns
-  wp1Glb.position.set(-70, 0, 50);
-  wp2Glb.position.set(-50, 0, 50);
-  wp3Glb.position.set(-30, 0, 50);
-  wp4Glb.position.set(-10, 0, 50);
-  wp5Glb.position.set(10, 0, 50);
-  wp6Glb.position.set(30, 0, 50);
-  wp7Glb.position.set(50, 0, 50);
-  wp8Glb.position.set(70, 0, 50);
-  //black pawns
-  bp1Glb.position.set(-70, 0, -50);
-  bp2Glb.position.set(-50, 0, -50);
-  bp3Glb.position.set(-30, 0, -50);
-  bp4Glb.position.set(-10, 0, -50);
-  bp5Glb.position.set(10, 0, -50);
-  bp6Glb.position.set(30, 0, -50);
-  bp7Glb.position.set(50, 0, -50);
-  bp8Glb.position.set(70, 0, -50);
-}
-setAllPos();
+glb = await loader.loadAsync('public/rook.glb');
+scene.add(br1Glb = glb.scene);
+glb = await loader.loadAsync('public/rook.glb');
+scene.add(br2Glb = glb.scene);
+glb = await loader.loadAsync('public/knight.glb');
+scene.add(bn1Glb = glb.scene);
+glb = await loader.loadAsync('public/knight.glb');
+scene.add(bn2Glb = glb.scene);
+glb = await loader.loadAsync('public/bishop.glb');
+scene.add(bb1Glb = glb.scene);
+glb = await loader.loadAsync('public/bishop.glb');
+scene.add(bb2Glb = glb.scene);
+glb = await loader.loadAsync('public/queen.glb');
+scene.add(bqGlb = glb.scene);
+glb = await loader.loadAsync('public/king.glb');
+scene.add(bkGlb = glb.scene);
+setAllPos(true);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 camera.position.y = 50; camera.position.z = 50;
@@ -93,11 +179,10 @@ function animate(time) {
 renderer.setAnimationLoop(animate);
 
 
-
 //resets the local data
 function resetLocal() {
   wPlayerName = "Justin";
-  wp1 = (-70, 0, 50);
+  wp1 = "a2";
   wp2 = "b2";
   wp3 = "c2";
   wp4 = "d2";
@@ -138,10 +223,11 @@ async function setServer() {
   const blob = await fetch(`https://chessonlinedata.netlify.app/.netlify/functions/sessionData${params}&set=true`);
 }
 
-//set local data
+//set local data, then positions
 function setLocal(json) {
   console.log(json);
   localData = json;
+  setAllPos(false);
 }
 
 //get server data
@@ -150,4 +236,6 @@ async function getServer() {
   const json = await blob.json();
   setLocal(json);
 }
+resetLocal();
+setServer();
 getServer();
